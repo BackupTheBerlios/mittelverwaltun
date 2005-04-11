d7627d2137ecbf0280608a590b3b94fb9bb4e142
menzimuck <menzimuck>
2005-04-11 08:26:52 +0000

    *** empty log message ***

--- mittelverwaltun/gui/AbschlussHaushaltsjahr.java
+++ mittelverwaltun/gui/AbschlussHaushaltsjahr.java
@@ -14,6 +14,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 
 public class AbschlussHaushaltsjahr extends JInternalFrame implements ActionListener, TableModelListener {
